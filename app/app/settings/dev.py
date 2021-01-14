5ede17edf33b1da020836e3472868098a7c39ea8
Ali Mehasin <alimehasin@gmail.com>
2021-01-14 17:07:45 +0300

    Settings up a local postgresql db

--- app/app/settings/dev.py
+++ app/app/settings/dev.py
@@ -6,7 +6,11 @@ ALLOWED_HOSTS = "*"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRESQL_DB"),
+        "HOST": os.getenv("POSTGRESQL_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRESQL_PORT", 5432),
+        "USER": os.getenv("POSTGRESQL_USER"),
+        "PASSWORD": os.getenv("POSTGRESQL_PASSWORD"),
     }
 }
