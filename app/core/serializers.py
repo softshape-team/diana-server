7c69b9b49f0af1e813e81d32cda64e7d5f38dfea
Ali Mehasin <alimehasin@gmail.com>
2021-01-18 15:04:57 +0300

    Add pk to readonly fields

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.fields import ReadOnlyField
 
 from . import models
 
@@ -8,4 +7,4 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Task
         fields = "__all__"
-        read_only_fields = ("user", "tags", "done_at")
+        read_only_fields = ("pk", "user", "tags", "done_at")
