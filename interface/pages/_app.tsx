a435fb29691c00727c14eebc74680e09574978af
Ali Mehasin <alimehasin@gmail.com>
2021-02-08 17:14:15 +0300

    Update diana welcome page

--- interface/pages/_app.tsx
+++ interface/pages/_app.tsx
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
--- interface/pages/index.tsx
+++ interface/pages/index.tsx
@@ -1,7 +1,40 @@
+import { Card, Container, Row, Col } from "react-bootstrap";
+import cn from "classnames";
+
+import style from "./main.module.scss";
+
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div>
+      <Container fluid>
+        <Row className="justify-content-center">
+          <Card as={Col} md={6} className={cn(style.card)}>
+            <Card.Header>
+              <h1>Welcome to Diana</h1>
+            </Card.Header>
+            <Card.Body>
+              Diana is a free and open-source application that helps you to live
+              a better day. <br />
+              To contribute on diana project checkout our source code
+              <ul className={style.sourceCodeLinks}>
+                <li>
+                  <a href="#">Diana web</a>
+                </li>
+                <li>
+                  <a href="#">Diana mobile</a>
+                </li>
+              </ul>
+            </Card.Body>
+            <Card.Footer>
+              Created by <a href="https://softshape.org">Softshape</a>
+            </Card.Footer>
+          </Card>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
