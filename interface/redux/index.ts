ee59948b84c6fd4cdf303790436f961f8c7944ab
Ali Mehasin <alimehasin@gmail.com>
2021-02-14 14:53:06 +0300

    Add redux dev tools and update redux logger to log in dev only

--- interface/redux/index.ts
+++ interface/redux/index.ts
@@ -1,12 +1,21 @@
 import { createStore, applyMiddleware } from "redux";
 import { MakeStore, createWrapper, Context } from "next-redux-wrapper";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 import reducer from "./reducers";
 
-// TODO: apply logger middleware only on development environment
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
 const makeStore: MakeStore = (context: Context) => {
-  return createStore(reducer, applyMiddleware(thunk, logger));
+  return createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middlewares))
+  );
 };
 
 const wrapper = createWrapper(makeStore, { debug: true });
