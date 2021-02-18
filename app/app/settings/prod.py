892b4c2d06efca72c71f327f4e4ac6b825d15547
Ali Mehasin <alimehasin@gmail.com>
2021-02-18 11:18:34 +0300

    Add CORS_ALLOWED_ORIGINS to settings.prod

--- app/app/settings/prod.py
+++ app/app/settings/prod.py
@@ -3,3 +3,5 @@ from .base import *
 DEBUG = os.getenv("DEBUG", 0)
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(" ")
