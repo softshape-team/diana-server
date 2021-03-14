3426a7efe5ed7c8b65b923037b0a8e72f3565467
Ali Mehasin <alimehasin@gmail.com>
2021-03-14 17:18:28 +0300

    Add SECURE_PROXY_SSL_HEADER to settings/prod.py and set X-Forwarded-Proto to https in nginx.conf

--- app/app/settings/prod.py
+++ app/app/settings/prod.py
@@ -5,3 +5,5 @@ DEBUG = os.getenv("DEBUG", 0)
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(" ")
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
