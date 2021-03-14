5950b2f7cd09b489a20d52f594fe14b8b08e2d36
Ali Mehasin <alimehasin@gmail.com>
2021-03-14 20:17:58 +0300

    Serve mediafiles in development

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -27,3 +29,6 @@ urlpatterns = [
         name="redoc",
     ),
 ]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
